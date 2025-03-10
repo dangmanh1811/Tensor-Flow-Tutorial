9b39b18981dfd38e575fa10de7c425936c23c6fb
dangmanh <dangmanh1811@gmail.com>
2025-03-11 01:22:42 +0700

    first commit

---
+++ 05.RNNs_GRUs_LSTMs_Bidirectional.py
@@ -0,0 +1,12 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(-1, 28*28).astype('float32') / 255.
+x_test = x_test.reshape(-1, 28*28).astype('float32') / 255.
